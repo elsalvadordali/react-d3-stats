ece4754b4c022d68c6178eb1f48d5f750e33839a
elsalvadordali <tloochich@icloud.com>
2022-09-05 06:15:44 -0400

    sort works

--- src/data/Ohio.tsx
+++ src/data/Ohio.tsx
@@ -21,7 +21,7 @@ type Info = {
 
 const Ohio = () => {
     const [data, setData] = useState<Info[]|null|undefined>(null)
-    const [dropdown, setDropdown] = useState<string>('close')
+    const [dropdown, setDropdown] = useState<string>('open')
     useEffect(() => {
         if (!data) {
         fetch('https://datausa.io/api/data?Geography=04000US39&measure=Average%20Wage,Average%20Wage%20Appx%20MOE,Total%20Population,Total%20Population%20MOE%20Appx,Record%20Count&drilldowns=Gender&Employment%20Time%20Status=1&Detailed%20Occupation=291141,533030,1191XX,537062,434051&Record%20Count>=5&year=latest')
@@ -46,7 +46,7 @@ const Ohio = () => {
         //let a = data.sort((a: Info,b: Info) => a["Detailed Occupation"].localeCompare(b["Detailed Occupation"]))
         let ohio = d3.select('#ohio')  
             .attr('class', 'hello?')
-            .attr('width', 500)
+            .attr('width', 700)
             .attr('height', height)
         
             ohio.selectAll('rect')
@@ -54,38 +54,42 @@ const Ohio = () => {
             .enter()
             .append('rect')
             .attr('y', (d: Info, i: number) => i * 30)
-            .attr('x', (d: Info, i: number) => (300 - d['Average Wage'] / 250))
+            .attr('x', (d: Info, i: number) => (380 - d['Average Wage'] / 250))
             .attr('fill', (d: Info, i: number) => d.Gender == 'Male' ? '#a0c7fe' : '#dafea4')
             .attr('width', (d: Info, i: number) => (d['Average Wage'] / 250))
             .attr('height', 25)
 
-            ohio.selectAll('text')
+            ohio.selectAll('text.title')
             .data(data)
             .enter()
             .append('text')
-            .attr('class', 'tooltip')
-            .text((d: Info) => d.Gender === 'Male' ? d['Detailed Occupation'] : '')
-            //.attr('transform', 'rotate(-90) ')
-            .attr('x', 305)
+            .attr('class', 'wage')
+            .text((d: Info) => d['Average Wage'].toFixed(0))
+            .attr('x', (d: Info, i: number) => 330 - d['Average Wage'] / 250)
+            .attr('y', (d: Info, i: number) => 18 + (i * 30))
             .attr('fill', '#162802')
-            .attr('y', (d: Info, i: number) => 30 + (i * 30))
-            .attr('text-align', 'right')
 
-            //ohio.selectAll('text')
-            //.data(a)
+            ohio.selectAll('text.value')
+            .data(data)
+            .enter()
+            .append('text')
+            .attr('class', 'occupation')
+            .text((d: Info) => d['Detailed Occupation'])
+            .attr('x', 385)
+            .attr('y', (d: Info, i: number) => 20 + (i * 30))
+            .attr('fill', '#162802')
 
 
-            const xScale = d3.scaleLinear()
-            .domain([0, d3.max(data, (d) => d[0])])
-            .range([25, 25]);
-            const xAxis = d3.axisBottom(xScale);
     }
 
     function sortBy(measure: string) {
-        if (measure === 'occupation') setData((prev) => prev?.sort((a,b) => a['Detailed Occupation'].localeCompare(b['Detailed Occupation'])))
-        else if (measure === 'wage') setData((prev) => prev?.sort((a,b) => a['Average Wage'] - b['Average Wage']))
-        else if (measure === 'gender') setData((prev) => prev?.sort((a,b) => a['ID Gender'] - b['ID Gender']))
-        reDraw()
+        if (measure === 'occupation') setData((prev: Info[]) => [... prev?.sort((a,b) => a['Detailed Occupation'].localeCompare(b['Detailed Occupation']))])
+        else if (measure === 'wage') setData((prev: Info[]) => [...prev?.sort((a,b) => a['Average Wage'] - b['Average Wage'])])
+        else if (measure === 'gender') setData((prev: Info[]) => [... prev?.sort((a,b) => a['ID Gender'] - b['ID Gender'])])
+        let ohio = d3.select('#ohio') 
+        ohio.selectAll('rect').remove()
+        ohio.selectAll('text').remove()
+        //reDraw()
     }
 
     if (data) {
@@ -98,9 +102,9 @@ const Ohio = () => {
         <div onClick={() => setDropdown(dropdown === 'close' ? 'open' : 'close')} className={dropdown}>
             <h6>{dropdown}</h6>
             <div>
-            <button onClick={() => sortBy('occupation')}>by Occupation</button>
-            <button onClick={() => sortBy('wage')}>by Wage</button>
-            <button onClick={() => sortBy('gender')}>by Gender</button>
+                <button onClick={() => sortBy('occupation')}>by Occupation</button>
+                <button onClick={() => sortBy('wage')}>by Wage</button>
+                <button onClick={() => sortBy('gender')}>by Gender</button>
             </div>
         </div>
         </>
